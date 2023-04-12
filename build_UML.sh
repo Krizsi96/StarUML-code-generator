376450f8e2bf6a20a9f8a2d062e5d15e9a14ca46
Kristóf Kovács <94800908+Krizsi96@users.noreply.github.com>
2023-04-12 23:20:24 +0200

    feat: add header file generator to the script

--- build_UML.sh
+++ build_UML.sh
@@ -3,3 +3,8 @@ CYAN='\033[36m'
 NC='\033[m'
 echo -e "${CYAN}generate diagrams form UML model${NC}"
 staruml image models/bank.mdj -f jpeg -o "out/<%=filenamify(element.name)%>_diagram.jpeg"
+
+echo ""
+
+echo -e "${CYAN}generate header files from UML model${NC}"
+staruml ejs models/bank.mdj -t ejs/cpp-class.ejs -s "@UMLClass" -o "out/<%=filenamify(element.name)%>.h"
