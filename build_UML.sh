1d753f33ec7eb76420e2087c6f6d74cdcf832e2e
Kristóf Kovács <94800908+Krizsi96@users.noreply.github.com>
2023-04-13 00:10:48 +0200

    feat: add variable for the model location

--- build_UML.sh
+++ build_UML.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 CYAN='\033[36m'
 NC='\033[m'
+SOURCE_MODEL='wiki/src/system_design.mdj'
+
 echo -e "${CYAN}generate diagrams form UML model${NC}"
-staruml image models/bank.mdj -f jpeg -o "out/<%=filenamify(element.name)%>_diagram.jpeg"
+staruml image ${SOURCE_MODEL} -f jpeg -o "wiki/src/diagrams/<%=filenamify(element.name)%>_diagram.jpeg"
 
 echo ""
 
 echo -e "${CYAN}generate header files from UML model${NC}"
-staruml ejs models/bank.mdj -t ejs/cpp-class.ejs -s "@UMLClass" -o "out/<%=filenamify(element.name)%>.h"
+staruml ejs ${SOURCE_MODEL} -t .ejs/cpp-class.ejs -s "@UMLClass" -o "Arduino_Pedalbox/src/<%=filenamify(element.name)%>.h"
