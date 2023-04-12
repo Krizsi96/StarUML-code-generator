1dec361cf714632b0bfa0aa05ef5290a0d28cf77
Kristóf Kovács <94800908+Krizsi96@users.noreply.github.com>
2023-04-12 23:21:41 +0200

    refactor: remove windows script files

--- code_generator.ps1
+++
@@ -1,5 +0,0 @@
-# Create alias for staruml
-Set-Alias -Name staruml -Value 'C:\Program Files\StarUML\StarUML.exe'
-
-# Call EJS for code generation
-staruml ejs models/bank.mdj -t ejs/cpp-class.ejs -s "@UMLClass" -o "out/<%=filenamify(element.name)%>.h"
--- diagram_generator.ps1
+++
@@ -1,5 +0,0 @@
-# Create alias for staruml
-Set-Alias -Name staruml -Value 'C:\Program Files\StarUML\StarUML.exe'
-
-# Call EJS for code generation
-staruml image models/bank.mdj -f jpeg -o "out/<%=filenamify(element.name)%>_diagram.jpeg"
