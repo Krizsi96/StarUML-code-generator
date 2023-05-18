dcde8e5f9d5735ff90f7dc17cfd2740151ddb3b0
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-05-18 15:01:01 +0200

    use header object in the ejs file

--- .ejs/code-generator.js
+++ .ejs/code-generator.js
@@ -1,17 +1,24 @@
 const { collectUMLEnumerations } = require('./enum_declaration.js');
 
-let header = {
-  CollectElements: function (UMLelement) {
-    attributes = UMLelement.attributes;
-    methods = UMLelement.methods;
-    enumerations = collectUMLEnumerations(UMLelement);
-  },
-  attributes: [],
-  methods: [],
-  enumerations: []
-};
 
 function CodeGenerator(UMLelement) {
+  let header = {
+    CollectElements: function (UMLelement) {
+      attributes = UMLelement.attributes;
+      methods = UMLelement.methods;
+      enumerations = collectUMLEnumerations(UMLelement);
+    },
+    attributes: [],
+    methods: [],
+    enumerations: [],
+    log: [],
+    supers: [],
+    extends: '',
+  };
+
+  header.supers = UMLelement.getGeneralElements();
+  header.extends = header.supers.length > 0 ? ': public ' + header.supers[0].name : '';
+
   header.CollectElements(UMLelement);
   return header;
 }
