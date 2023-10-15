55a5080aed5f2de4a6ccade56f70d951332c56e7
Kristóf Kovács <kristof.kovacs1996@gmail.com>
2023-10-15 13:20:56 +0100

    REFACTOR:: remove old shell scripts

--- build_UML.ps1
+++
@@ -1,5 +0,0 @@
-Set-Alias -Name staruml -Value 'C:\Program Files\StarUML\StarUML.exe'
-
-# Call EJS for code generation
-staruml ejs models/system_design.mdj -t ejs/cpp-class.ejs -s "@UMLClass" -o "out/<%=filenamify(element.name)%>.hpp"
-staruml ejs models/system_design.mdj -t ejs/cpp-class.ejs -s "@UMLInterface" -o "out/<%=filenamify(element.name)%>.hpp"
